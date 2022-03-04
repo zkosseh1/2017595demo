3bbf40de7d561e6992301723e21cbaa6be21934d
Zanah Kosseh <2017595@csl-student.wlv.ac.uk>
2022-03-04 18:14:44 +0000

    initial push

---
+++ app/Controllers/Users.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class Users extends BaseController
+{
+    public function login()
+    {
+        print('welcome to the login page');
+    }
+	
+	public function logout() 
+	{
+		print("welcome to the logout page");
+	}
+	
+}
