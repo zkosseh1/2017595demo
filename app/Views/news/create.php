8326203b791d3608c9fb3a5e179219ac67ba314e
Zanah Kosseh <2017595@csl-student.wlv.ac.uk>
2022-03-29 23:11:53 +0100

    minor improvements made to create page

--- app/Views/news/create.php
+++ app/Views/news/create.php
@@ -7,20 +7,20 @@
     <?= csrf_field() ?>
 
 	<div class="mb-3">
-		<label for="title" class="form-label">Article Title</label>
+		<label for="title" class="form-label">Recipe Title</label>
 		<input class="form-control" type="input" name="title" /><br />
 	</div>
 	
 	<div class="mb-3">
-		<label for="body" class="form-label">Article Text</label>
+		<label for="body" class="form-label">Recipe Text</label>
 		<textarea input class="form-control" name="body" cols="45" rows="4"></textarea><br />
 	</div>
 	
 	<div class="mb-3">
-		<label for="body" class="form-label">Article Image Link</label>
+		<label for="body" class="form-label">Recipe Image Link</label>
 		<textarea input class="form-control" name="images" cols="45" rows="2"></textarea><br />
 	</div>
 	
 	
-    <input class="btn btn-outline-success" type="submit" name="submit" value="Post New Article" />
+    <input class="btn btn-outline-success" type="submit" name="submit" value="Post New Recipe" />
 </form>
